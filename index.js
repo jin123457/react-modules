c5ca46d3a315abc6492c0639b09a6006d170a434
jin123457 <jinsol9909@gmail.com>
2025-05-06 21:59:17 +0900

    Updates

--- index.js
+++ index.js
@@ -1,186 +1,140 @@
-import { useState } from "react";
-const validation = {
-  isNumber: (value) => {
-    const regex = /^[0-9]+$/;
-    return regex.test(value);
+import { useState as u } from "react";
+const o = {
+  isNumber: (e) => /^[0-9]+$/.test(e),
+  isValidLength: (e, r) => e.length === r,
+  isValidMonth: (e) => {
+    const r = Number(e);
+    return !(r < 1 || r > 12);
   },
-  isValidLength: (value, length) => {
-    return value.length === length;
+  isValidYear: (e) => {
+    const r = Number(e);
+    return !((/* @__PURE__ */ new Date()).getFullYear() % 100 > r);
   },
-  isValidMonth: (value) => {
-    const numericMonth = Number(value);
-    if (numericMonth < 1 || numericMonth > 12) {
-      return false;
-    }
-    return true;
-  },
-  isValidYear: (value) => {
-    const numericYear = Number(value);
-    const currentYear = (/* @__PURE__ */ new Date()).getFullYear() % 100;
-    if (currentYear > numericYear) {
-      return false;
-    }
-    return true;
-  },
-  isValidateDate: (month, year) => {
-    const numericMonth = Number(month);
-    const numericYear = Number(year);
-    const currentYear = (/* @__PURE__ */ new Date()).getFullYear() % 100;
-    if (currentYear === numericYear) {
-      const currentMonth = (/* @__PURE__ */ new Date()).getMonth() + 1;
-      if (currentMonth > numericMonth) {
-        return false;
-      }
-    }
-    return true;
+  isValidateDate: (e, r) => {
+    const a = Number(e), s = Number(r);
+    return !((/* @__PURE__ */ new Date()).getFullYear() % 100 === s && (/* @__PURE__ */ new Date()).getMonth() + 1 > a);
   }
-};
-const validateCardNumber = (cardNumber) => {
-  const errorState = {
-    first: false,
-    second: false,
-    third: false,
-    fourth: false
+}, c = (e) => {
+  const r = {
+    first: !1,
+    second: !1,
+    third: !1,
+    fourth: !1
   };
-  let errorMessage = "";
-  for (const [k, value] of Object.entries(cardNumber)) {
-    const key = k;
-    if (!validation.isNumber(value) && value !== "") {
-      errorState[key] = true;
-      errorMessage = "숫자만 입력하세요.";
+  let a = "";
+  for (const [s, t] of Object.entries(e)) {
+    const n = s;
+    if (!o.isNumber(t) && t !== "") {
+      r[n] = !0, a = "숫자만 입력하세요.";
       break;
     }
-    if (!validation.isValidLength(value, 4) && value !== "") {
-      errorState[key] = true;
-      errorMessage = "4자리 숫자를 입력하세요.";
+    if (!o.isValidLength(t, 4) && t !== "") {
+      r[n] = !0, a = "4자리 숫자를 입력하세요.";
       break;
     }
   }
-  return { errorState, errorMessage };
-};
-const initialCardNumberState = {
+  return { errorState: r, errorMessage: a };
+}, l = {
   first: "",
   second: "",
   third: "",
   fourth: ""
 };
-function useCardNumber() {
-  const [cardNumber, setCardNumber] = useState(initialCardNumberState);
-  const handleCardNumberChange = (e, field) => {
-    const value = e.target.value;
-    setCardNumber((prev) => ({ ...prev, [field]: value }));
+function m() {
+  const [e, r] = u(l), a = (s, t) => {
+    const n = s.target.value;
+    r((i) => ({ ...i, [t]: n }));
   };
   return {
-    cardNumber,
-    errorState: validateCardNumber(cardNumber),
-    handleCardNumberChange
+    cardNumber: e,
+    errorState: c(e),
+    handleCardNumberChange: a
   };
 }
-const validateCvc = (cvc) => {
-  const errorObject = {
-    errorState: false,
+const f = (e) => {
+  const r = {
+    errorState: !1,
     errorMessage: ""
   };
-  if (!validation.isNumber(cvc) && cvc !== "") {
-    return { errorState: true, errorMessage: "숫자만 입력하세요." };
-  }
-  if (!validation.isValidLength(cvc, 3) && cvc !== "") {
-    return { errorState: true, errorMessage: "3자리 숫자를 입력하세요." };
-  }
-  return errorObject;
+  return !o.isNumber(e) && e !== "" ? { errorState: !0, errorMessage: "숫자만 입력하세요." } : !o.isValidLength(e, 3) && e !== "" ? { errorState: !0, errorMessage: "3자리 숫자를 입력하세요." } : r;
 };
-function useCvc() {
-  const [cvc, setCvc] = useState("");
-  const handleCvcChange = (e) => {
-    const value = e.target.value;
-    setCvc(value);
+function v() {
+  const [e, r] = u(""), a = (s) => {
+    const t = s.target.value;
+    r(t);
   };
   return {
-    cvc,
-    errorState: validateCvc(cvc),
-    handleCvcChange
+    cvc: e,
+    errorState: f(e),
+    handleCvcChange: a
   };
 }
-const validateExpiration = (expiration) => {
-  const errorState = {
-    year: false,
-    month: false
+const d = (e) => {
+  const r = {
+    year: !1,
+    month: !1
   };
-  let errorMessage = "";
-  for (const [k, value] of Object.entries(expiration)) {
-    const key = k;
-    if (!validation.isNumber(value) && value !== "") {
-      errorState[key] = true;
-      errorMessage = "숫자만 입력하세요.";
+  let a = "";
+  for (const [s, t] of Object.entries(e)) {
+    const n = s;
+    if (!o.isNumber(t) && t !== "") {
+      r[n] = !0, a = "숫자만 입력하세요.";
       break;
     }
-    if (!validation.isValidLength(value, 2) && value !== "") {
-      errorState[key] = true;
-      errorMessage = "2자리 숫자를 입력하세요.";
+    if (!o.isValidLength(t, 2) && t !== "") {
+      r[n] = !0, a = "2자리 숫자를 입력하세요.";
       break;
     }
-    if (!validation.isValidMonth(value) && key === "month" && value !== "") {
-      errorState[key] = true;
-      errorMessage = "유효한 월을 입력하세요.";
+    if (!o.isValidMonth(t) && n === "month" && t !== "") {
+      r[n] = !0, a = "유효한 월을 입력하세요.";
       break;
     }
-    if (!validation.isValidYear(value) && key === "year" && value !== "") {
-      errorState[key] = true;
-      errorMessage = "지나지 않은 연도를 입력해주세요.";
+    if (!o.isValidYear(t) && n === "year" && t !== "") {
+      r[n] = !0, a = "지나지 않은 연도를 입력해주세요.";
       break;
     }
-    if (!validation.isValidateDate(expiration.month, expiration.year) && expiration.month !== "" && expiration.year !== "") {
-      errorState[key] = true;
-      errorMessage = "지나지 않은 날짜를 입력해주세요.";
+    if (!o.isValidateDate(e.month, e.year) && e.month !== "" && e.year !== "") {
+      r[n] = !0, a = "지나지 않은 날짜를 입력해주세요.";
       break;
     }
   }
-  return { errorState, errorMessage };
-};
-const initialExpirationState = {
+  return { errorState: r, errorMessage: a };
+}, h = {
   year: "",
   month: ""
 };
-function useExpiration() {
-  const [expiration, setExpiration] = useState(initialExpirationState);
-  const handleExpirationChange = (e, field) => {
-    const value = e.target.value;
-    setExpiration((prev) => ({ ...prev, [field]: value }));
+function M() {
+  const [e, r] = u(h), a = (s, t) => {
+    const n = s.target.value;
+    r((i) => ({ ...i, [t]: n }));
   };
   return {
-    expiration,
-    errorState: validateExpiration(expiration),
-    handleExpirationChange
+    expiration: e,
+    errorState: d(e),
+    handleExpirationChange: a
   };
 }
-const validatePassword = (password) => {
-  const errorObject = {
-    errorState: false,
+const g = (e) => {
+  const r = {
+    errorState: !1,
     errorMessage: ""
   };
-  if (!validation.isNumber(password) && password !== "") {
-    return { errorState: true, errorMessage: "숫자만 입력하세요." };
-  }
-  if (!validation.isValidLength(password, 2) && password !== "") {
-    return { errorState: true, errorMessage: "2자리 숫자를 입력하세요." };
-  }
-  return errorObject;
+  return !o.isNumber(e) && e !== "" ? { errorState: !0, errorMessage: "숫자만 입력하세요." } : !o.isValidLength(e, 2) && e !== "" ? { errorState: !0, errorMessage: "2자리 숫자를 입력하세요." } : r;
 };
-function usePassword() {
-  const [password, setPassword] = useState("");
-  const handlePasswordChange = (e) => {
-    const value = e.target.value;
-    setPassword(value);
+function N() {
+  const [e, r] = u(""), a = (s) => {
+    const t = s.target.value;
+    r(t);
   };
   return {
-    password,
-    errorState: validatePassword(password),
-    handlePasswordChange
+    password: e,
+    errorState: g(e),
+    handlePasswordChange: a
   };
 }
 export {
-  useCardNumber,
-  useCvc,
-  useExpiration,
-  usePassword
+  m as useCardNumber,
+  v as useCvc,
+  M as useExpiration,
+  N as usePassword
 };
